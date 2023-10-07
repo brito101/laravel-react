157ea2107e547e2bf671198a608e7d089227722b
brito101 <rodrigobrito101@hotmail.com>
2023-10-07 15:14:29 -0300

    ADD initial style

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Commands used
 
-`curl -s https://laravel.build/react | bash`
-`sail npm install --save-dev @vitejs/plugin-react`
-`sail npm install react@latest react-dom@latest`
+- `curl -s https://laravel.build/react | bash`
+- `sail npm install --save-dev @vitejs/plugin-react`
+- `sail npm install react@latest react-dom@latest`
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Roboto&display=swap" rel="stylesheet">
 
     <title>Home</title>
 </head>
@@ -11,7 +14,7 @@
 <body>
     <div id="app"></div>
     @viteReactRefresh
-    @vite('resources/js/main.jsx')
+    @vite(['resources/js/main.jsx', 'resources/css/app.css'])
 </body>
 
 </html>
