6d9f29da5d64de190566a791d2ee52c8c42a9a77
brito101 <rodrigobrito101@hotmail.com>
2024-01-20 19:59:19 -0300

    form introduction

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,14 +1,43 @@
 import React from "react";
-import Product from "./Product";
-import { GlobalStorage } from "./GlobalContext";
-import Clear from "./Clear";
 
 const App = () => {
+    // const [name, setName] = React.useState("");
+    // const [email, setEmail] = React.useState("");
+    const [form, setForm] = React.useState({
+        name: "",
+        email: "",
+    });
+
+    function handleChange({ target }) {
+        const { id, value } = target;
+        setForm({ ...form, [id]: value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(form.name, form.email);
+    }
+
     return (
-        <GlobalStorage>
-            <Product />
-            <Clear />
-        </GlobalStorage>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+                id="name"
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+            />
+            <label htmlFor="email">E-mail</label>
+            <input
+                id="email"
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+            />
+            <button>Enviar</button>
+        </form>
     );
 };
 
