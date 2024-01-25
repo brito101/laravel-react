6de41329a4b45a60cec9adb1d8aaaa436dba1368
brito101 <rodrigobrito101@hotmail.com>
2024-01-25 17:01:11 -0300

    Forms challenge

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,42 +1,88 @@
 import React from "react";
+import Radio from "./Form/Radio";
+
+const questions = [
+    {
+        question: "Qual método é utilizado para criar componentes?",
+        options: [
+            "React.makeComponent()",
+            "React.createComponent()",
+            "React.createElement()",
+        ],
+        answer: "React.createElement()",
+        id: "p1",
+    },
+    {
+        question: "Como importamos um componente externo?",
+        options: [
+            'import Component from "./Component"',
+            'require("./Component")',
+            'import "./Component"',
+        ],
+        answer: 'import Component from "./Component"',
+        id: "p2",
+    },
+    {
+        question: "Qual hook não é nativo?",
+        options: ["useEffect()", "useFetch()", "useCallback()"],
+        answer: "useFetch()",
+        id: "p3",
+    },
+    {
+        question: "Qual palavra deve ser utilizada para criarmos um hook?",
+        options: ["set", "get", "use"],
+        answer: "use",
+        id: "p4",
+    },
+];
 
 const App = () => {
-    // const [name, setName] = React.useState("");
-    // const [email, setEmail] = React.useState("");
-    const [form, setForm] = React.useState({
-        name: "",
-        email: "",
+    const [answers, setAnswers] = React.useState({
+        p1: "",
+        p2: "",
+        p3: "",
+        p4: "",
     });
+    const [slide, setSlide] = React.useState(0);
+    const [result, setResult] = React.useState(null);
 
     function handleChange({ target }) {
-        const { id, value } = target;
-        setForm({ ...form, [id]: value });
+        setAnswers({ ...answers, [target.id]: target.value });
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(form.name, form.email);
+    function finalResult() {
+        const corrects = questions.filter(
+            ({ id, answer }) => answers[id] === answer
+        );
+        setResult(`Você acertou: ${corrects.length} de ${questions.length}`);
+    }
+
+    function handleClick() {
+        if (slide < questions.length - 1) {
+            setSlide(slide + 1);
+        } else {
+            setSlide(slide + 1);
+            finalResult();
+        }
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-                id="name"
-                type="text"
-                name="name"
-                value={form.name}
-                onChange={handleChange}
-            />
-            <label htmlFor="email">E-mail</label>
-            <input
-                id="email"
-                type="email"
-                name="email"
-                value={form.email}
-                onChange={handleChange}
-            />
-            <button>Enviar</button>
+        <form onSubmit={(e) => e.preventDefault()}>
+            {questions.map((question, index) => (
+                <Radio
+                    active={slide === index}
+                    key={index}
+                    onChange={handleChange}
+                    value={answers[question.id]}
+                    {...question}
+                />
+            ))}
+
+            {result ? (
+                <p>{result}</p>
+            ) : (
+                <button onClick={handleClick}>Próxima</button>
+            )}
         </form>
     );
 };
